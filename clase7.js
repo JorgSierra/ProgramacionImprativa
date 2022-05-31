ace4b5b5284ee534ca412697a678087f07604f7e
JorgSierra <jorg.sierrar@gmail.com>
2022-05-30 19:10:35 -0500

    pirmer commit

---
+++ clase7.js
@@ -0,0 +1,72 @@
+/*let edad = 20;
+
+if (edad < 18){
+    console.log("Puede pasar al bar.");
+}
+else if (edad < 21){
+    console.log("Puede pasar al bar, per no puede tomar alcohol.");
+}
+else if (edad < 0){
+    console.log("Error, edad inválida. Por favor ingrese un número válido");
+}
+else if (edad == 21){
+    console.log("Felicidades eres mayor a 21!!");
+}
+else{
+    console.log("Puede pasar al bar y tomar alcohol.");
+}*/
+
+/*
+function secreto(intento)
+{
+    let key = Math.ceil(Math.random() * 10);
+    
+    if(intento == key){
+        console.log("Felicitaciones!!!");
+    }
+    else{
+        console.log(">----Rayos!!!----<")
+        console.log("El numero secreto era:" + key);
+        console.log("Tu ingresaste: " + intento);
+    }
+
+}
+
+secreto(1);
+
+*/
+
+/*
+function abrirParacaidas(velocidad, altura){
+    if (velocidad < 1000 && altura >= 2000 && altura <=3000){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(abrirParacaidas(999, 1999));
+*/
+
+/*
+function traductor(palabra){
+    switch (palabra) {
+        case "casa":
+            console.log("House");
+            break;
+        case "pelota":
+            console.log("Ball");
+            break;
+        case "arbol":
+            console.log("Tree");
+            break;            
+        case "genio":
+            console.log("genious");
+            break;
+        default:
+            console.log("Me no comprendo!")
+            break;
+    }
+}
+*/
